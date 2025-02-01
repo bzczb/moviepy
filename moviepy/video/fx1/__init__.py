459a4972b397680eac2c9425ff26cab64e9b1276
vermont <uqzx@protonmail.com>
2025-01-31 20:22:55 -0500

    Add back old FX

---
+++ moviepy/video/fx1/__init__.py
@@ -0,0 +1,64 @@
+# import every video fx function
+
+from moviepy.video.fx1.accel_decel import accel_decel
+from moviepy.video.fx1.blackwhite import blackwhite
+from moviepy.video.fx1.blink import blink
+from moviepy.video.fx1.crop import crop
+from moviepy.video.fx1.even_size import even_size
+from moviepy.video.fx1.fadein import fadein
+from moviepy.video.fx1.fadeout import fadeout
+from moviepy.video.fx1.freeze import freeze
+from moviepy.video.fx1.freeze_region import freeze_region
+from moviepy.video.fx1.gamma_corr import gamma_corr
+from moviepy.video.fx1.headblur import headblur
+from moviepy.video.fx1.invert_colors import invert_colors
+from moviepy.video.fx1.loop import loop
+from moviepy.video.fx1.lum_contrast import lum_contrast
+from moviepy.video.fx1.margin import margin
+from moviepy.video.fx1.mask_and import mask_and
+from moviepy.video.fx1.mask_color import mask_color
+from moviepy.video.fx1.mask_or import mask_or
+from moviepy.video.fx1.mirror_x import mirror_x
+from moviepy.video.fx1.mirror_y import mirror_y
+from moviepy.video.fx1.multiply_color import multiply_color
+from moviepy.video.fx1.multiply_speed import multiply_speed
+from moviepy.video.fx1.painting import painting
+from moviepy.video.fx1.resize import resize
+from moviepy.video.fx1.rotate import rotate
+from moviepy.video.fx1.scroll import scroll
+from moviepy.video.fx1.supersample import supersample
+from moviepy.video.fx1.time_mirror import time_mirror
+from moviepy.video.fx1.time_symmetrize import time_symmetrize
+
+
+__all__ = (
+    "accel_decel",
+    "blackwhite",
+    "blink",
+    "crop",
+    "even_size",
+    "fadein",
+    "fadeout",
+    "freeze",
+    "freeze_region",
+    "gamma_corr",
+    "headblur",
+    "invert_colors",
+    "loop",
+    "lum_contrast",
+    "margin",
+    "mask_and",
+    "mask_color",
+    "mask_or",
+    "mirror_x",
+    "mirror_y",
+    "multiply_color",
+    "multiply_speed",
+    "painting",
+    "resize",
+    "rotate",
+    "scroll",
+    "supersample",
+    "time_mirror",
+    "time_symmetrize",
+)
